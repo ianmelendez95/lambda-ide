33f01500f699a897b4cad1431f0f6a03497f719a
Ian Melendez <imelendez@preludedynamics.com>
2022-04-11 22:14:54 -0500

    editor ref, simple value log

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react'
-import Editor from './monaco/Editor'
+import Editor, { EditorRef } from './monaco/Editor'
 import './style.css'
 
+
 export default function App(): React.ReactElement {
+  const editorRef: EditorRef = React.useRef(null)
+
+  function onClickButton() {
+    if (editorRef.current == null) {
+      console.warn("Editor not loaded")
+      return
+    }
+
+    console.log("Content: ", editorRef.current.getValue())
+  }
+
   return (
     <div style={{ height: "100vh", 
                   width: "100vw",
                   display: "flex",
                   flexFlow: "row nowrap" }}>
       <div style={{ height: "100%", width: "50%" }}>
-        <Editor/>
+        <Editor editorRef={editorRef}/>
       </div>
-      <div></div>
+      <div>
+        <input id="compile-button" type="button" onClick={onClickButton}/>
+      </div>
     </div>
   )
 }
--- src/monaco/Editor.tsx
+++ src/monaco/Editor.tsx
@@ -2,31 +2,45 @@ import * as React from "react";
 import * as monaco from 'monaco-editor'
 import { initVimMode } from 'monaco-vim'
 
-export default function Editor(): React.ReactElement {
-  const editorRef: React.MutableRefObject<HTMLDivElement> = React.useRef(null)
-  const statusBarRef: React.MutableRefObject<HTMLDivElement> = React.useRef(null)
+const INITIAL_TEXT = '(\\foo. foo bar) (\\baz. baz)'
+
+export type EditorRef = React.MutableRefObject<monaco.editor.IStandaloneCodeEditor>
+
+interface Props {
+  /**
+   * A ref to the Monaco editor instance.
+   * The 'current' property will be set with the editor once it has been initialized.
+   */
+  editorRef: EditorRef
+}
+
+export default function Editor({ editorRef }: Props): React.ReactElement {
+  console.debug("Rendering editor")
+  
+  const editorDivRef: React.MutableRefObject<HTMLDivElement> = React.useRef(null)
+  const statusDivRef: React.MutableRefObject<HTMLDivElement> = React.useRef(null)
 
   React.useEffect(() => {
-    if (editorRef.current && statusBarRef.current) {
-      setupMonaco(editorRef.current, statusBarRef.current)
+    if (editorDivRef.current && statusDivRef.current) {
+      editorRef.current = setupMonaco(editorDivRef.current, statusDivRef.current)
     } else {
       console.error("Element refs not set")
-      console.error("Editor: ", editorRef)
-      console.error("Status: ", statusBarRef)
+      console.error("Editor: ", editorDivRef)
+      console.error("Status: ", statusDivRef)
     }
-  })
+  }, [editorDivRef.current, statusDivRef.current])
 
   return (
     <div style={{ display: "flex",
                   height: "100%",
                   flexFlow: "column nowrap" }}>
       <div id="container" 
-           ref={editorRef}
+           ref={editorDivRef}
            style={{ border: "1px solid #ccc",
                     flexGrow: 1 }}>
       </div>
       <div id="status-bar" 
-           ref={statusBarRef}
+           ref={statusDivRef}
            style={{ height: 20,
                     border: "1px solid #ccc" }}>
       </div>
@@ -35,14 +49,15 @@ export default function Editor(): React.ReactElement {
 }
 
 function setupMonaco(editorElement: HTMLDivElement,
-                     statusBarElement: HTMLDivElement) {
+                     statusBarElement: HTMLDivElement): monaco.editor.IStandaloneCodeEditor {
+  console.debug("Setting up editor")
   const editor: monaco.editor.IStandaloneCodeEditor = monaco.editor.create(editorElement, {
-    value: initialText,
+    value: INITIAL_TEXT,
     language: 'lambda',
     theme: 'vs-dark'
   });
 
   initVimMode(editor, statusBarElement)
-}
 
-const initialText = `(\\foo. foo bar) (\\baz. baz)`
+  return editor
+}
