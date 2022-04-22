d4a482aad7745bce400d535c87c2d7223e426368
Ian Melendez <imelendez@preludedynamics.com>
2022-04-22 00:10:31 -0500

    pprint: added alternate test case

--- test/pprint.ts
+++ test/pprint.ts
@@ -17,6 +17,20 @@ describe('pprint', function () {
       ].join('\n'), showTree(testTree()))
     })
   })
+  describe('#showTreeAlt', function () {
+    it('should properly indent test tree', function () {
+      assert.equal([
+        'aaa[',
+        '  bbbbb[ccc,',
+        '        dd],',
+        '  eee,',
+        '  ffff[gg,',
+        '       hhh,',
+        '       ii]',
+        ']'
+      ].join('\n'), showTreeAlt(testTree()))
+    })
+  })
 })
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -28,6 +42,30 @@ describe('pprint', function () {
 type Tree = {
   value: string,
   children: Tree[]
+}
+
+/**
+ * aaa[bbbbb[ccc, 
+ *           dd],
+ * eee,
+ * ffff[gg,
+ *      hhh,
+ *      ii]]
+ */
+function testTree(): Tree {
+  return {
+    value: "aaa",
+    children: [{
+      value: "bbbbb", children: [{ value: "ccc", children: [] },
+      { value: "dd", children: [] }]
+    },
+    { value: "eee", children: [] },
+    {
+      value: "ffff", children: [{ value: "gg", children: [] },
+      { value: "hhh", children: [] },
+      { value: "ii", children: [] }]
+    }]
+  }
 }
 
 /**
@@ -71,25 +109,43 @@ function showTrees(ts: Tree[]): Doc {
 }
 
 /**
- * aaa[bbbbb[ccc, 
- *           dd],
- * eee,
- * ffff[gg,
- *      hhh,
- *      ii]]
+ * showTree’ (Node s ts) = text s <> showBracket’ ts
  */
-function testTree(): Tree {
-  return {
-    value: "aaa",
-    children: [{
-      value: "bbbbb", children: [{ value: "ccc", children: [] },
-      { value: "dd", children: [] }]
-    },
-    { value: "eee", children: [] },
-    {
-      value: "ffff", children: [{ value: "gg", children: [] },
-      { value: "hhh", children: [] },
-      { value: "ii", children: [] }]
-    }]
+function showTreeAlt({ value, children }: Tree): Doc {
+  const s = value
+  const ts = children
+  return concat(text(s), showBracketAlt(ts))
+}
+
+/**
+ * showBracket’ [] = nil
+ * showBracket’ ts = text "[" <>
+ *                   nest 2 (line <> showTrees’ ts) <>
+ *                   line <> text "]")
+ */
+function showBracketAlt(ts: Tree[]): Doc {
+  return ts.length === 0
+    ? nil()
+    : concat(text("["),
+             nest(2, concat(line(), showTreesAlt(ts))),
+             line(), text("]"))
+}
+
+/**
+ * showTrees’ [t] = showTree t
+ * showTrees’ (t:ts) = showTree t <> text "," <> line <> showTrees ts
+ */
+function showTreesAlt(ts: Tree[]): Doc {
+  if (ts.length === 0) {
+    throw new Error('Expecting at least one tree')
+  } else if (ts.length === 1) {
+    return showTree(ts[0])
+  } else {
+    return concat(
+      showTree(ts[0]),
+      text(","),
+      line(),
+      showTrees(ts.slice(1))
+    )
   }
 }
